6d9e682c3a713c23726d7f226e3beec1d0ed58ed
hamitmizrak <hamitmizrak@gmail.com>
2022-02-05 19:16:52 +0300

    ComputerEntity.java revize

--- src/main/java/com/innova/entity/ComputerEntity.java
+++ src/main/java/com/innova/entity/ComputerEntity.java
@@ -12,7 +12,6 @@ public class ComputerEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column()
     private Long computerId;
 
     @Column(name = "computer_name")
@@ -22,23 +21,23 @@ public class ComputerEntity implements Serializable {
     @Column(name = "computer_trade")
     private String computerTrade;
 
-    @Column(name = "computer_price",columnDefinition = "Decimal(10,2) default='100.00'")
+    @Column(name = "computer_price")
+    //@Column(name = "computer_price",columnDefinition = "Decimal(10,2) default='100.00'")
     private String computerPrice;
-
-    @Lob
-    private String bigData;
-
-    @Transient
-    //sadece java var ancak database eklenmeyecek
-    private String justJava;
-
-    @Column(name = "computer_serial_number",length = 11,nullable = false,unique = true)
-    private int serialComputerNumber;
 
     @Column(name = "created_date")
     @Temporal(TemporalType.TIMESTAMP)
     @CreationTimestamp
     private Date createdDate;
+
+//    @Lob  //buyuk veriler icin
+//    private String bigData;
+
+//    @Transient  //sadece java var ancak database eklenmeyecek
+//    private String justJava;
+
+//    @Column(name = "computer_serial_number",length = 11,nullable = false,unique = true)
+//    private int serialComputerNumber;
 
 
 }

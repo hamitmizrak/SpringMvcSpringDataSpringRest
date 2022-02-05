3be97c158d4692e172c2bd54d6d590c38d5efa33
hamitmizrak <hamitmizrak@gmail.com>
2022-02-05 16:36:10 +0300

    Initial commit

---
+++ src/main/java/com/innova/SpringMvcSpringDataSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.innova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSpringDataSpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSpringDataSpringRestApplication.class, args);
+    }
+
+}

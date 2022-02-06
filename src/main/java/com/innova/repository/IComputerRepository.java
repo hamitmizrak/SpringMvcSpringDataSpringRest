8e5443cff618a191330973086684d3a6a4f50722
hamitmizrak <hamitmizrak@gmail.com>
2022-02-06 17:32:01 +0300

    bütün @AutoWired tek bir yapı olarak kurguladık

--- src/main/java/com/innova/repository/IComputerRepository.java
+++ src/main/java/com/innova/repository/IComputerRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface IComputerRepository extends CrudRepository<ComputerEntity,Long> {
+public interface IComputerRepository extends CrudRepository<ComputerEntity,Long>,IMyRepository {
 
     //1.YOL
     //Register MVC thymeleaf, Entity register, Database ekleyeğiz

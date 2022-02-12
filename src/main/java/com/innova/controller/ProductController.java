0d0d4418a87ad26b5f1be8b427ccf32c2c6bb8e3
hamitmizrak <hamitmizrak@gmail.com>
2022-02-12 13:15:03 +0300

    list Client

--- src/main/java/com/innova/controller/ProductController.java
+++ src/main/java/com/innova/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.innova.controller;
 
 import com.innova.dto.ProductDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.Locale;
 
 @Controller
+@Log4j2
 public class ProductController {
 
     // http://localhost:8080/client/controller/string
@@ -48,6 +52,22 @@ public class ProductController {
         ResponseEntity<ProductDto> responseEntity=restTemplate.exchange(URL, HttpMethod.GET, HttpEntity.EMPTY,ProductDto.class);
         ProductDto productDto2=responseEntity.getBody();
         return "client resten veri aldı: "+productDto2;
+    }
+
+
+    // http://localhost:8080/client/controller/productdto/specialList/
+    @GetMapping("/client/controller/productdto/specialList/")
+    @ResponseBody
+    public List<ProductDto>  getProductServicesResponseEntityList() {
+        String URL = "http://localhost:8080/rest/list/";
+        RestTemplate restTemplate = new RestTemplate();
+        //Anonymous Class
+        ResponseEntity<List<ProductDto>> responseEntity=restTemplate.exchange(URL, HttpMethod.GET, HttpEntity.EMPTY, new ParameterizedTypeReference<List<ProductDto>>() {});
+        List<ProductDto> list=responseEntity.getBody();
+        for (ProductDto temp:list){
+            log.info(temp);
+        }
+        return list;
     }
 
 

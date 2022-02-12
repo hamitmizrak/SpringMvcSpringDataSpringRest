017f2542789b3c5eed0b2bd745bbc0f0aa3422e3
hamitmizrak <hamitmizrak@gmail.com>
2022-02-12 19:39:19 +0300

    revize

--- src/main/java/com/innova/security/_08_AddUserEncrypedBean.java
+++ src/main/java/com/innova/security/_08_AddUserEncrypedBean.java
@@ -9,23 +9,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+//@EnableWebSecurity
 public class _08_AddUserEncrypedBean extends WebSecurityConfigurerAdapter {
 
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                .inMemoryAuthentication()//bellek üzereinde olsun database düzeyde olmasın
-                .withUser("root") //kullanıc adı
-                .password(passwordEncoder().encode("root"))//şifrelenmiş datatdır artık
-                .roles("USER");
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder
+//                .inMemoryAuthentication()//bellek üzereinde olsun database düzeyde olmasın
+//                .withUser("root") //kullanıc adı
+//                .password(passwordEncoder().encode("root"))//şifrelenmiş datatdır artık
+//                .roles("USER");
+//    }
 
 }
---
+++ src/main/java/com/innova/security/_09_SecurityAuthentication.java
@@ -0,0 +1,33 @@
+package com.innova.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class _09_SecurityAuthentication extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http
+               .authorizeRequests()
+               .antMatchers("/security/public","/security/success") //bu @GetMapping izin verdiğim url
+               .permitAll()//yukarıdaki asyfalara login olmadan izin ver
+               .anyRequest()
+               .authenticated()
+               .and()
+               .formLogin();
+    }
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .withUser("root")
+                .password("{noop}root")
+                .roles("USER");
+    }
+}
